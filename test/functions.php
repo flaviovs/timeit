736ad035b4519e5a91db59b1c5c7d9e37c95b9f7
Flávio Veloso <flaviovs@magnux.com>
2016-06-10 20:59:10 -0700

    Add basic unit tests

---
+++ test/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+class FunctionsTestcase extends PHPUnit_Framework_TestCase {
+
+	public function testTimeIt() {
+		$res = timeit('usleep(200000)', NULL, 10);
+
+		$this->assertEquals(10, $res[0]);
+		$this->assertEquals(0.200, $res[1], '', 0.001);
+		$this->assertRegExp('/^200\.\d\dms$/', $res[2]);
+	}
+
+	public function testTimeItRepeat() {
+		$res = timeit_repeat('', NULL, 10);
+		$this->assertEquals(3, count($res));
+	}
+
+	public function testTimeItStr() {
+		$this->assertRegExp('/^: 10 loops, best of 3: \d\d\.\d\d\S+ per loop$/',
+		                    timeit_str('', NULL, 10));
+	}
+}
